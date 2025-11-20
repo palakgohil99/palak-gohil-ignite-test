e3149cdf1296464378a03d9514782a05f7fcae1c
Palak Gohil <palak.gohil6792@gmail.com>
2025-11-20 18:18:31 +0530

    Fix filename

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import './bootstrap';
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './app.vue'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
